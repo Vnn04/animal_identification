19200e4199593beb6179d38cc3d73955e8bd8d29
quyencanh203 <quyencanh203@gmail.com>
2024-03-18 00:14:38 +0700

    tao html file sign in upbase

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
+
+# @app.route('/login', methods=['POST', 'GET'])
+# def login():
+#     if request.method == 'POST':
+#         uname = request.form['']
+#         if uname:
+#             return 
+#     return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

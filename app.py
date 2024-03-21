182064ce46fab24cda640cb74d0e24e96141b38e
quyencanh203 <quyencanh203@gmail.com>
2024-03-21 17:34:13 +0700

    uoas

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
 
-@app.route('/')
+# trang home
+@app.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('index1.html')
 
-# @app.route('/login', methods=['POST', 'GET'])
-# def login():
-#     if request.method == 'POST':
-#         uname = request.form['']
-#         if uname:
-#             return 
-#     return render_template('login.html')
+
+# trang login
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        uname = request.form['uname']
+        psw = request.form['psw']
+        if uname and psw is not None:
+            return redirect(url_for('home'))
+    return render_template('login.html')
+
+# trang sign up
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port='9999', debug=True)
